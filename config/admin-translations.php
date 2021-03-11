75ac7427bab74bd0be87150bcac64008d1e880af
Leandro A. Ibarra <libarra@fusap.com.ar>
2021-03-11 13:21:25 -0300

    [Boilerplate] Fix Craftable install for first time

--- config/admin-translations.php
+++ config/admin-translations.php
@@ -30,11 +30,6 @@ return [
         app_path(),
         resource_path('views'),
         // here you can add your own directories
-        // base_path('routes'), // uncomment if you have translations in your routes i.e. you have localized URLs
-        base_path('vendor/brackets/admin-auth/src'),
-        base_path('vendor/brackets/admin-auth/resources'),
-        base_path('vendor/brackets/admin-ui/resources'),
-        base_path('vendor/brackets/admin-translations/resources'),
     ],
 
 ];
